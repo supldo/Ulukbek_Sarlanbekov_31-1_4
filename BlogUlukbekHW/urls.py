f35b0ea329a99af9c88b1aa5e2af6e5a9913a379
SarUlukbek <sar.ulukbek@gmail.com>
2023-08-23 12:23:43 +0600

    HW4

--- BlogUlukbekHW/urls.py
+++ BlogUlukbekHW/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('categories/', views.categories_view)
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
